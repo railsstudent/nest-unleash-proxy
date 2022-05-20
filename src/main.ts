c39950e0bdf0f29e90f2d27b77df1439133fb768
Connie Leung <connie.leung@diginex.com>
2022-05-20 12:35:02 +0800

    feat: implement unleash proxy server

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,33 @@ import * as express from 'express';
 import * as compression from 'compression';
 import * as morgan from 'morgan';
 import * as helmet from 'helmet';
+import { ApiConfigService } from './core';
+import unleashProxy = require('@unleash/proxy');
+import * as cors from 'cors';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const nestApp = await NestFactory.create(AppModule);
+  const configService = nestApp.get(ApiConfigService);
+  const logger = new Logger('Main');
+
+  const app = unleashProxy.createApp({
+    unleashUrl: configService.featureToggleUrl,
+    unleashApiToken: configService.featureToggleApiToken,
+    clientKeys: [configService.featureToggleClientKeys],
+    environment: configService.appEnv,
+  });
+
+  app.use(cors());
   app.use(helmet());
   app.use(morgan('combined'));
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ limit: '10mb', extended: true }));
   app.use(compression());
-  await app.listen(4000);
+  await app.listen(configService.portNumber, () => {
+    logger.log(
+      `Unleash Proxy listening on http://localhost:${configService.portNumber}/proxy`,
+    );
+  });
 }
 bootstrap();
